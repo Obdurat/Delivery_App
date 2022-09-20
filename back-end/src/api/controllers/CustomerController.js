9f4f423d7dcfb4d2e2100ed7e8672d3a9e482510
Anderson Rodrigues <omicron_rpl@hotmail.com>
2022-09-20 19:27:26 -0300

    docs: :memo: Added comment to controller subclass

--- back-end/src/api/controllers/CustomerController.js
+++ back-end/src/api/controllers/CustomerController.js
@@ -4,6 +4,7 @@ class CustomerController extends BaseController {
     constructor(service) {
         super(service);
     }
+    // Metodos Adicionais vem na sub-classe aqui
 }
 
 module.exports = CustomerController;
