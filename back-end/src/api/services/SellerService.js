42bcf642acf9d1c3adcbb636005782ac94f1fe5a
roberio <roberio-almeida@hotmail.com>
2022-09-21 01:17:29 -0300

    feat: create seller service and method getAll

---
+++ back-end/src/api/services/SellerService.js
@@ -0,0 +1,20 @@
+const BaseService = require('./BaseService');
+const CustomError = require('../errors/CustomError');
+
+class SellerService extends BaseService {
+  constructor(model) {
+      super(model);
+  }
+
+  async getAll(seller_id) {
+    const sales = await this.model.findAll({
+      where: { seller_id },
+    });
+
+    if(!sales) throw new CustomError('Sale not found', 404);    
+
+    return sales;
+  }
+};
+
+module.exports = SellerService;
