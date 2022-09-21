e8e6661b5fd3f6daf8a31480f97481c8c4362f57
prtpj1 <prtpj1@gmail.com>
2022-09-21 16:29:41 -0300

    :construction: InProgress:(Requisite 6) - add endpoint /POST/register

--- back-end/src/api/services/CustomerService.js
+++ back-end/src/api/services/CustomerService.js
@@ -3,5 +3,4 @@ const BaseService = require('./BaseService');
 class CustomerService extends BaseService {
     // assim como no controller metodos adicionais virão aqui
 }
-
 module.exports = CustomerService;
