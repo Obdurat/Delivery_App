466d4a3617e0e21dfeed1a8bb0cddf4f84da4f04
prtpj1 <prtpj1@gmail.com>
2022-09-22 18:00:58 -0300

    :construction: InProgress:(Requisite 38 and 39) add endpoint POST/admin/manage :arrow_right: user admin cad add new users with new field "role" and cant add an user which already exist

--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const registerRouter = require('./registerRouter');
 const customerRouter = require('./customerRouter');
 const sellerRouter = require('./sellerRouter');
+const adminRouter = require('./adminRouter');
 
 const Endpoints = express.Router();
 
 Endpoints.use('/register', registerRouter);
 Endpoints.use('/customer', customerRouter);
 Endpoints.use('/seller', sellerRouter);
+Endpoints.use('/admin', adminRouter);
 
 module.exports = Endpoints;
