29550a2f705ba54e853048ad83328f6f8476db0e
roberio <roberio-almeida@hotmail.com>
2022-09-23 02:46:30 -0300

    chore: create and initial config for OrderCard, for sellers

--- front-end/src/pages/Seller/index.jsx
+++ front-end/src/pages/Seller/index.jsx
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from 'react';
 
 const Seller = () => {
-  const [orders, setOrders] = useState([]);
+  const [orders, setOrders] = useState([{
+    id: 1,
+    userId: 1,
+    sellerId: 2,
+    totalPrice: "2.20",
+    deliveryAdress: "street",
+    deliveryNumber: "12345",
+    saleDate: "2001-08-01T00:00:00.000Z",
+    status: true,
+  }]);
 
   useEffect(() => {
    // Checa usuario e envia requisição para a api
