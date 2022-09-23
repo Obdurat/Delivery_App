2f828486b845ab8458c2049276d9a13e8989fb0a
roberio <roberio-almeida@hotmail.com>
2022-09-23 05:06:57 -0300

    chore: create services folder and api file
    config request for seller orders route api

---
+++ front-end/src/services/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const URL = 'http://localhost:3001';
+
+const reqSellerOrders = async () => {
+  try {
+    const { data } = await axios.get(`${URL}/seller/orders`);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export {
+  reqSellerOrders,
+};
