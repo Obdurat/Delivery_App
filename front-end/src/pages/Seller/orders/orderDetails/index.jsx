b8089e91a35ca9933ed4914a1a29f6d8ffcbf3e2
roberio <roberio-almeida@hotmail.com>
2022-09-24 01:06:25 -0300

    chore: more config for orderDetails page

--- front-end/src/pages/Seller/orders/orderDetails/index.jsx
+++ front-end/src/pages/Seller/orders/orderDetails/index.jsx
@@ -1,8 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function OrderDetails() {
+  const [order, setOrder] = useState({});
+
+  const { id } = useParams();
+  const { user } = useAuth();
+
   useEffect(() => {
-  }, []);
+    if (user.token) {
+      (async () => {
+        const res = await ProviderApi.getSellerOrders(id);
+        if (res.success) {
+          setOrder(res.data);
+        }
+      })();
+    }
+  }, [user, id]);
 
   return (
     <>
