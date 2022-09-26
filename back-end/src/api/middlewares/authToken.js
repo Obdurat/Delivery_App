4b9d32d1bfa63bd5523ebb983c0ea9312da4ee73
prtpj1 <prtpj1@gmail.com>
2022-09-26 20:35:21 -0300

    :wrench: fix: debugger configs :arrow_right: correção do caminho ./jwt.evaluation.key

--- back-end/src/api/middlewares/authToken.js
+++ back-end/src/api/middlewares/authToken.js
@@ -3,8 +3,10 @@ const fs = require('fs');
 const path = require('path');
 const CustomError = require('../errors/CustomError');
 
-const SECRET = fs.readFileSync(path
-  .resolve('./back-end/jwt.evaluation.key'), { encoding: 'utf-8' });
+const SECRET = fs.readFileSync(
+  path.resolve('./jwt.evaluation.key'),
+  { encoding: 'utf-8' },
+);
 
 const authToken = (req, _res, next) => {
   const token = req.headers.authorization || '';
--- back-end/src/api/utils/tokenGenerator.js
+++ back-end/src/api/utils/tokenGenerator.js
@@ -2,8 +2,10 @@ const { sign } = require('jsonwebtoken');
 const fs = require('fs');
 const path = require('path');
 
-const SECRET = fs.readFileSync(path
-  .resolve('./back-end/jwt.evaluation.key'), { encoding: 'utf-8' });
+const SECRET = fs.readFileSync(
+  path.resolve('./jwt.evaluation.key'),
+  { encoding: 'utf-8' },
+);
 
 const tokenGenerator = (payload) => {
   const jwtConfig = {
