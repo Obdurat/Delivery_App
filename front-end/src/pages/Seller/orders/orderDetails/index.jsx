788fca1f3b092f3d310605afdb2eeeeed8527c6d
roberio <roberio-almeida@hotmail.com>
2022-09-28 02:03:18 -0300

    refactor: add useEffect on onderDetails

--- front-end/src/pages/Seller/orders/orderDetails/index.jsx
+++ front-end/src/pages/Seller/orders/orderDetails/index.jsx
@@ -1,14 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useSales, SalesContext } from '../../../../context/providers/SalesProvider';
+import { useSales } from '../../../../context/providers/SalesProvider';
 import Header from '../../../../components/Header';
 import DetailsCard from '../components/DetailsCard';
 
 export default function OrderDetails() {
-  const { orderDetails } = useSales();
-  const salesContext = useContext(SalesContext);
+  const { orderDetails, setOrderId } = useSales();
   const { id } = useParams();
-  salesContext.setOrderId(id);
+
+  useEffect(() => {
+    setOrderId(id);
+  }, [setOrderId, id]);
 
   return (
     <>
