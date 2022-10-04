7f66212035cdd367a375c287de432f091d5cc4dc
roberio <roberio-almeida@hotmail.com>
2022-10-04 14:51:20 -0300

    test: config routes for MSW and create mocks files

--- front-end/src/tests/mocks/server/handlers.js
+++ front-end/src/tests/mocks/server/handlers.js
@@ -1,7 +1,12 @@
 import { rest } from 'msw';
 import { getUser } from '../../../utils/localStorage';
 import allUsers from '../adminMock';
-import sales from '../customerMock';
+import {
+  sales,
+  users,
+  checkoutResponse,
+  customerOrderDetails,
+} from '../customerMock';
 import products from '../productsMock';
 import { response } from '../registerMock';
 import { orders, orderDetails } from '../sellerMock';
@@ -28,9 +33,21 @@ const handlers = [
     ctx.status(code.OK),
     ctx.json(allUsers),
   )),
+  rest.get(`${BASE_URL}/customer`, (_req, res, ctx) => res(
+    ctx.status(code.OK),
+    ctx.json(users),
+  )),
+  rest.post(`${BASE_URL}/customer/checkout`, (_req, res, ctx) => res(
+    ctx.status(code.OK),
+    ctx.json(checkoutResponse),
+  )),
   rest.get(`${BASE_URL}/customer/sales`, (_req, res, ctx) => res(
     ctx.status(code.OK),
     ctx.json(sales),
+  )),
+  rest.get(`${BASE_URL}/customer/sales/:id`, (_req, res, ctx) => res(
+    ctx.status(code.OK),
+    ctx.json(customerOrderDetails),
   )),
   rest.get(`${BASE_URL}/seller/orders`, (_req, res, ctx) => res(
     ctx.status(code.OK),
